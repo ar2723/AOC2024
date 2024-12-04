dda1b3805f41f31eeaa4596e27e50cb980b61c2f
syjeong <sparrowr2723@gmail.com>
2024-12-04 13:43:10 +0900

    12/04 3일차 문제 답안 코드 정리

--- AOC2024_03.js
+++ AOC2024_03.js
@@ -1,47 +1,54 @@
-import { readFile } from 'fs';
+// node js 파일 처리 모듈 import
+const fs = require('fs');
 
-function mul(a, b) {
-    return a * b;
-}
+// 글로벌 함수 선언
+mul = (a, b) =>  a * b;
 
-function findFn() {
-    readFile('./AOC2024_03_INPUT.txt', 'utf-8', function(err, data) {
+function arrangeMemory() {
+    // maxLine 토큰화 이슈로 인해 로컬파일로 읽어와서 스트링으로 변환
+    fs.readFile('./AOC2024_03_INPUT.txt', 'utf-8', function(err, data) {
         if (err) {
             console.error("Error reading file:", err);
             return;
         }
         const inputStr = data.toString();
+        // mul(num,num) 형태의 문자열 모두 검색하기 위한 정규식
         const regEx = /(mul)\((\d+),(\d+)\)/g;
-        const matchArr = inputStr.matchAll(regEx);
-    
+        const matchArr = [...inputStr.matchAll(regEx)];
         let answer = 0;
-    
+        
+        // match = ['mul(num,num)', 'mul', 'num', 'num']
         matchArr.forEach(match => {
-            const arg1 = parseInt(match[2], 10); // 첫 번째 인자
-            const arg2 = parseInt(match[3], 10); // 두 번째 인자
+            const arg1 = parseInt(match[2], 10);
+            const arg2 = parseInt(match[3], 10);
     
-            answer += mul(arg1, arg2); // 함수 호출
-            // console.log(`${funcName}(${arg1}, ${arg2}) = ${result}`);
+            answer += mul(arg1, arg2);
         });
-        console.log(answer);
+        console.log('arrangeMemory :', answer);
     });
 }
 
-// findFn();
+arrangeMemory();
 
-function findFn2() {
-    readFile('./AOC2024_03_INPUT.txt', 'utf-8', function(err, data) {
+function arrangeMemory2() {
+    fs.readFile('./AOC2024_03_INPUT.txt', 'utf-8', function(err, data) {
         if (err) {
             console.error("Error reading file:", err);
             return;
         }
         const inputStr = data.toString();
+        // mul(num,num), do(), don't() 형태의 문자열 모두 검색하기 위한 정규식
         const regEx = /(mul)\((\d+),(\d+)\)|do\(\)|don't\(\)/g;
-        const matchArr = inputStr.matchAll(regEx);
+        const matchArr = [...inputStr.matchAll(regEx)];
     
         let answer = 0;
+
+        // do(), don't 나오기 전에 초기 함수는 활성화 상태
         let isEnabled = true;
 
+        // match = ['mul(num,num)', 'mul', 'num', 'num']
+        // match = ['do()']
+        // match = ['don't()']
         matchArr.forEach(match => {
             // console.log(match[0], match.index);
 
@@ -49,13 +56,14 @@ function findFn2() {
             else if(match[0] == `don't()`) isEnabled = false;
 
             if(isEnabled && match[0].substr(0,3) == 'mul') {
-                const arg1 = parseInt(match[2], 10); // 첫 번째 인자
-                const arg2 = parseInt(match[3], 10); // 두 번째 인자
-                answer += mul(arg1, arg2); // 함수 호출
+                const arg1 = parseInt(match[2], 10); 
+                const arg2 = parseInt(match[3], 10);
+
+                answer += mul(arg1, arg2);
             }
         });
-        console.log(answer);
+        console.log('arrangeMemory2 :', answer);
     });
 }
 
-findFn2();
+arrangeMemory2();
