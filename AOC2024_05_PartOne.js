1ac7acbfa0c3d9707f64cdf03428d5a2e1c41a8b
syjeong <sparrowr2723@gmail.com>
2024-12-06 10:06:10 +0900

    12/06 5일차 문제 답안 코드 작성 완료_PartTwo까지 클리어

--- AOC2024_05_PartOne.js
+++ AOC2024_05_PartOne.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// 검증용 페이지 리스트 txt 파일 읽어와서 배열로 변환
 fs.readFile('./AOC2024_05_INPUT_One.txt', 'utf-8', function(err, data) {
     if (err) {
         console.error("Error reading file:", err);
@@ -8,6 +9,7 @@ fs.readFile('./AOC2024_05_INPUT_One.txt', 'utf-8', function(err, data) {
     const arr = data.split('\n').map(i => i.split("|"));
 
     if(arr != null) {
+        // 검증대상 페이지 리스트 txt 파일 읽어와서 배열로 변환
         fs.readFile('./AOC2024_05_INPUT_Two.txt', 'utf-8', function(err, data) {
             if (err) {
                 console.error("Error reading file:", err);
@@ -19,14 +21,20 @@ fs.readFile('./AOC2024_05_INPUT_One.txt', 'utf-8', function(err, data) {
 
             dataArr.forEach(el => {
                 let isRightOrderedArr = true;
+                
                 for(let i in arr) {
+                    // 각각 페이지 리스트에서 각각 검증 페이지 순서 별로 인덱스를 찾은 다음
                     const firstPageIdx = el.indexOf(arr[i][0]);
                     const secondPageIdx = el.indexOf(arr[i][1]);
+                    // 두 개 페이지 모두 페이지 리스트 안에 존재하는 경우
+                    // 첫번째 페이지 인덱스가 두번째 페이지 인덱스 보다 큰 경우 = 첫번째 페이지가 뒤에 있는 경우
                     if(firstPageIdx >= 0 && secondPageIdx >= 0 && firstPageIdx > secondPageIdx) {
+                        // 해당 페이지 리스트는 제대로 정렬되어지 있지 않음
                         isRightOrderedArr = false;
                         break;
                     }
                 }
+                // 페이지 리스트가 모든 페이지 순서에 대해 검증을 통과했다면, 통과된 페이지 리스트의 가운데 원소를 더해줌
                 if(isRightOrderedArr) {
                     console.log(JSON.stringify(el));
                     answer += +el[parseInt(el.length / 2)];
--- AOC2024_05_PartTwo.js
+++ AOC2024_05_PartTwo.js
@@ -1,38 +1,68 @@
 const fs = require('fs');
 
-fs.readFile('./AOC2024_05_EXAMPLE_One', 'utf-8', function(err, data) {
+// 검증용 페이지 리스트 txt 파일 읽어와서 배열로 변환
+fs.readFile('./AOC2024_05_INPUT_One.txt', 'utf-8', function(err, data) {
     if (err) {
         console.error("Error reading file:", err);
         return;
     }
 
-    const arr = data.split('\r\n').map(i => i.split("|"));
+    const arr = data.split('\n').map(i => i.split("|"));
 
     if(arr != null) {
-        fs.readFile('./AOC2024_05_EXAMPLE_Two.txt', 'utf-8', function(err, data) {
+        // 검증대상 페이지 리스트 txt 파일 읽어와서 배열로 변환
+        fs.readFile('./AOC2024_05_INPUT_Two.txt', 'utf-8', function(err, data) {
             if (err) {
                 console.error("Error reading file:", err);
                 return;
             }
+
             let answer = 0;
+
             const dataArr = data.split("\r\n").map(i => i.split(","));
 
-            dataArr.forEach(el => {
+            /**
+             * 페이지리스트 검증 함수
+             * @param {Array} arrToBeChecked 검증할 페이지 리스트 
+             * @param {Array} compareArr 검증용 페이지 배열
+             * @returns boolean
+             */
+            function checkRightOrder(arrToBeChecked, compareArr) {
                 let isRightOrderedArr = true;
-                for(let i in arr) {
-                    const firstPageIdx = el.indexOf(arr[i][0]);
-                    const secondPageIdx = el.indexOf(arr[i][1]);
+                for(let i in compareArr) {
+                    const firstPageIdx = arrToBeChecked.indexOf(compareArr[i][0]);
+                    const secondPageIdx = arrToBeChecked.indexOf(compareArr[i][1]);
                     if(firstPageIdx >= 0 && secondPageIdx >= 0 && firstPageIdx > secondPageIdx) {
                         isRightOrderedArr = false;
-
+                        // 페이지 순서가 잘못되어 있는 경우
+                        // 페이지 리스트 내에서 두 페이지 위치를 바꿔 줌
+                        arrToBeChecked[firstPageIdx] = compareArr[i][1];
+                        arrToBeChecked[secondPageIdx] = compareArr[i][0];
+                        break;
                     }
                 }
-                if(isRightOrderedArr) {
-                    console.log(JSON.stringify(el));
-                    answer += +el[parseInt(el.length / 2)];
+                // 검증 결과 반환
+                return isRightOrderedArr;
+            }
+            
+            dataArr.forEach(el => {
+                const isRightOrderedArr = checkRightOrder(el, arr);
+                // 최초 페이지 리스트 정렬 순서 검증을 통과하지 못한 리스트에 한해서 수행
+                if(!isRightOrderedArr) {
+                    let isRevisedRightly = false;
+                    // 페이지 리스트 정렬 순서 검증 실패 -> 페이지 순서 재정렬 -> 페이지 리스트 재검증
+                    // 재검증 과정에서 검증을 통과할 때까지 계속 반복
+                    // 재검증 과정에서 검증 통과 시 가운데 원소 더해줌
+                    while(!isRevisedRightly) {
+                        isRevisedRightly = checkRightOrder(el, arr);
+                        console.log(JSON.stringify(el), isRevisedRightly);
+                    }
+                    if(isRevisedRightly) {
+                        answer += +el[parseInt(el.length / 2)];
+                    }
                 }
             })
-            console.log(answer)
+            console.log(answer);
         })
     } 
 })
