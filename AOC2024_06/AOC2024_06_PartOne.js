679e189aa111d6b32369384376dc4748edb54191
syjeong <sparrowr2723@gmail.com>
2024-12-06 17:33:52 +0900

    12/06 6일차 문제 답안 코드 작성_PartOne까지 클리어

---
+++ AOC2024_06/AOC2024_06_PartOne.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+fs.readFile('./AOC2024_06/AOC2024_06_INPUT.txt', 'utf-8', function(err, data) {
+    if (err) {
+        console.error("Error reading file:", err);
+        return;
+    }
+    const arr = data.split('\n').map(i => i.split(''));
+
+    let startPostion = [];
+
+    arr.forEach((el, idx)=> {
+        const colIdx = el.indexOf('^');
+        if(colIdx > 0) {
+            console.log(`row : ${idx} | column : ${colIdx}`);
+            startPostion = [...[idx, colIdx]];
+        }
+    })
+
+    let [row, col] = startPostion;
+
+    let direction = 'up'
+    
+    while(row < arr.length - 1 && col < arr[0].length - 1) {
+        switch (direction) {
+            case 'up' : 
+                row--;
+                if(arr[row][col] == '.') arr[row][col] = 'X'
+                else if (arr[row][col] == '#') {
+                    direction = 'right';
+                    row++;
+                }
+                break;
+
+            case 'right' : 
+                col++;
+                if(arr[row][col] == '.') arr[row][col] = 'X'
+                else if (arr[row][col] == '#') {
+                    direction = 'down';
+                    col--;
+                }
+                break;
+
+            case 'down' : 
+                row++;
+                if(arr[row][col] == '.') arr[row][col] = 'X'
+                else if (arr[row][col] == '#') {
+                    direction = 'left';
+                    row--;
+                }
+                break;
+
+            case 'left' : 
+                col--;
+                if(arr[row][col] == '.') arr[row][col] = 'X'
+                else if (arr[row][col] == '#') {
+                    direction = 'up';
+                    col++;
+                }
+                break;
+        }
+    }
+    let count = 0;
+    arr.forEach(el => {
+        console.log(el.join(''));
+        count += el.filter(i => i == 'X').length;
+    })
+    console.log(count);
+})
